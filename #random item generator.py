b5d28ac6c5283396c1e09ad5287deb290a3b6435
katyperrier <bengreywinters@gmail.com>
2025-07-01 05:59:38 -0500

    added first test

--- #random item generator.py
+++ #random item generator.py
@@ -1,10 +1,17 @@
+import random as r
+
+
 #random item generator
 #proof of concept
 
 # [Adjective] [Item] of [Substance]
 
-Adjective = ["Molten","Radiant","Gloomy","Ancient","Arcane","Venomous"
+adjective = ["Molten","Radiant","Gloomy","Ancient","Arcane","Venomous"
              ,"Heaven-forged","Hell-forged","Jewel-encrusted","Permafrosted"
              ,"Electric","Singing","Whistling","Humming","Crescent"]
-Item = ["Sword","Axe","Helmet","Spear","Gauntlet","Lance"]
-Substance = ["Fire","Ice","Lightning","Magic","Song"]
+item = ["Sword","Axe","Helmet","Spear","Gauntlet","Lance"]
+substance = ["Fire","Ice","Lightning","Magic","Song"]
+
+selectedAdjective = adjective[r.randint(0,4)]
+
+print(selectedAdjective)
