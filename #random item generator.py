d39a81fea76d4fd9a3329b820931d0286c1503e9
katyperrier <bengreywinters@gmail.com>
2025-07-01 05:31:56 -0500

    added basic lists of adjectives items and substances

--- #random item generator.py
+++ #random item generator.py
@@ -3,4 +3,8 @@
 
 # [Adjective] [Item] of [Substance]
 
-Adjective = ["Molten", "Crescent"]
+Adjective = ["Molten","Radiant","Gloomy","Ancient","Arcane","Venomous"
+             ,"Heaven-forged","Hell-forged","Jewel-encrusted","Permafrosted"
+             ,"Electric","Singing","Whistling","Humming","Crescent"]
+Item = ["Sword","Axe","Helmet","Spear","Gauntlet","Lance"]
+Substance = ["Fire","Ice","Lightning","Magic","Song"]
